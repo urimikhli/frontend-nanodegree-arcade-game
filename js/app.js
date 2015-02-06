a7a3a3eddef723222430896a8688a98f78fe5366
Uri Mikhli <urimikhli@gmail.com>
2015-02-05 19:54:29 -0500

    initial commit with gitignore and stub for Player function

--- js/app.js
+++ js/app.js
@@ -24,6 +24,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our Player uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+}
+
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//handle keypress
+Player.prototype.handleInput = function(allowedKeys) {
+
+}
 
 
 // Now instantiate your objects.
