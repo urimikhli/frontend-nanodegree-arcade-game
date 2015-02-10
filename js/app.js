e97940f889f46c384d49d847f0fe9348a440262a
Uri Mikhli <urimikhli@gmail.com>
2015-02-10 11:34:57 -0500

    auto move when holding down move buttons

--- js/app.js
+++ js/app.js
@@ -149,7 +149,7 @@ var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
